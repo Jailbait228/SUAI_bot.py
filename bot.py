239a5334b1841e5f84276058c9eef9b7628a9338
Jailbait228 <m.nac@yandex.ru>
2020-07-07 12:23:28 +0300

    bot changes

--- bot.py
+++ bot.py
@@ -2,9 +2,19 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-import random
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
 Bot = commands.Bot (command_prefix='!')
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json",scope)
+
+client = gspread.authorize(creds)
+
+
+#col = sheet.col_values(2)
 
 @Bot.event
 async def on_ready():
@@ -12,26 +22,62 @@ async def on_ready():
 
 @Bot.command(pass_context=True)
 async def start(ctx):
-    role = ctx.message.guild.get_role(387190022232735747)
+    role = ctx.message.guild.get_role(729285717628420106)
     await ctx.message.delete() 
     if role in ctx.message.author.roles:
         ctx1 = await ctx.send("Присутствующие")
         await ctx1.add_reaction("☑️")
-        await asyncio.sleep(5)
+        await asyncio.sleep(10)
         ctx1 = await ctx1.channel.fetch_message(ctx1.id)
         for reaction in ctx1.reactions:
             if reaction.emoji == "☑️":
                 positive = reaction
-       # async for user in positive.users().display_name:
-        #    await ctx.send('{0} has reacted with {1.emoji}!'.format(user, positive))
         users = await positive.users().flatten()
-        #winner = random.choice(users)
+        pprint(users)
         for i in users:
             if i.bot is False:
-                await ctx.send('{} has won the raffle.'.format(i.display_name))
+                name = i.display_name.split(' ')
+                day = 0
+                try: 
+                    check = name[2]
+                except:
+                    pprint('Error user {}'.format(name))
+                    continue
+                if name[2] == '1703':
+                    sheet = client.open("test").sheet1
+                    g = 3
+                    while g<7:
+                        cell = sheet.cell(g,2).value
+                        name1 = cell.split(' ')
+                        if name1[1] == name[0]:
+                            j = 11
+                            while True:
+                                cell_lesson = sheet.cell(g,j).value
+                                if cell_lesson == '-':
+                                    if day == 0:
+                                        day = j
+                                    sheet.update_cell(g,day, 1)
+                                    await asyncio.sleep(2)
+                                    break
+                                else:
+                                    j = j + 1
+                            break
+                        g = g + 1
+        g = 3
+        sheet = client.open("test").sheet1
+        while g<7:
+            cell = sheet.cell(g,day).value
+            if cell == "-":
+                sheet.update_cell(g,day, 0)
+                await asyncio.sleep(2)
+            g = g + 1   
+        await ctx1.delete() 
+        ctx1 = await ctx.send("`Выполнено`")
+        await asyncio.sleep(10)         
     else:
-        await ctx.send("`Нeдостаточно привилегий`")
+        ctx1 = await ctx.send("`Нeдостаточно привилегий`")
         await asyncio.sleep(10)
-    await ctx1.delete()    
+    await ctx1.delete() 
+       
 
-Bot.run("NzI4MjQzODQyNjE2MTMxNTk1.XwLx7w.7ul3bePpS3S827Oe5zyZMERe7zo")  
+Bot.run("NzI5MjY2NTgyMzIwMTg1Mzc1.XwOm_g.FMp3Zb7HPfBPUmeIukyk-8WYM7c")  
